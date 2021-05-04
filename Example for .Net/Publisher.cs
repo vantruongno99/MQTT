b0dac6b6e180fb95bc10e0d4639c1419b753c2cd
Fureshi <68948864+vantruongno99@users.noreply.github.com>
2021-05-04 17:28:22 +1000

    Add files via upload

---
+++ Example for .Net/Publisher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace Publisher
+{
+    class Publisher
+    {
+        static void Main(string[] args)
+        {
+            MqttClient client = new MqttClient("localhost");
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            while (true)
+            {
+                Random random = new Random();
+                int value = random.Next(1000);
+                string strValue = Convert.ToString(value) + "°C";
+                client.Publish("/home/temperature", Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                Thread.Sleep(1000);
+            }
+
+
+
+        }
+    }
+}
---
+++ Example for .Net/Subcriber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace Subcriber
+{
+    class Subcriber
+
+    {
+        static void Main(string[] args)
+        {
+
+
+            MqttClient client = new MqttClient("localhost");
+
+            // register to message received
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+
+           
+
+            // subscribe to the topic "/home/temperature" with QoS 2
+            client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
+
+            static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+            {
+                var msg = System.Text.Encoding.Default.GetString(e.Message);
+                System.Console.WriteLine("Message Received: " + msg);
+
+            }
+
+            
+
+        }
+    }
+}
